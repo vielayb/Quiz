05464d7b12d287e51bd113d1266f0c3f6266f662
Vielay B <vielayb@gmail.com>
2021-09-02 12:26:35 -0400

    Ben's Help

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,102 +1,9 @@
 var timerEl = document.getElementById("countdown");
 var startBtn = document.getElementById("start-btn");
-var nextBtn = document.getElementById("next-btn");
 var questionContainerEl = document.getElementById("question-container");
 var questionEl = document.getElementById("questions");
 var answersButtonEl = document.getElementById("answer-buttons");
-let shuffledQuestions, currentQuestionsIndex
-
-//Timer counting down from 100 seconds
-function countdown() {
-    var timeLeft = 50;
-
-    var timeInterval = setInterval(function() {
-if (timeLeft > 1) {
-    timerEl.textContent = timeLeft;
-    timeLeft--;
-
-} else if (timeLeft === 1) {
-    timeLeft.textContent = timeLeft + " Keep fighting!";
-    timeLeft--;
-} else {
-    timerEl.textContent = "You Lose";
-    clearInterval(timeInterval);
-
-}
-}, 1000);
-}
-
-startBtn.onclick = countdown;
-startBtn.addEventListener("click", startGame);
-nextBtn.addEventListener("click", () => {
-    currentQuestionsIndex++
-    setNextQuestion()
-})
-
-function startGame () {
-   startBtn.classList.add("hide");
-   shuffledQuestions = question.sort(() => Math.random() - 5);
-   currentQuestionsIndex = 0;
-   questionContainerEl.classList.remove("hide");
-   setNextQuestion();
-}
-
-function setNextQuestion () {
-    resetState();
-    showQuestion(shuffledQuestions[currentQuestionsIndex]);
-}
-
-function showQuestion(question) {
-    questionEl.innerText = question.question;
-    question.answers.forEach(answers => {
-        var button = document.createElement("button")
-        button.innerText = answers.text
-        button.classList.add("btn");
-        if (answers.correct) {
-            button.dataset.correct = answer. correct;
-        }
-        button.addEventListener("click", selectAnswer);
-        answersButtonEl.appendChild(button);
-    })
-}
-
-function resetState() {
-    nextBtn.classList.add("hide");
-    while (answersButtonEl.firstChild) {
-        answersButtonEl.removeChild
-        (answersButtonEl.firstChild)
-    }
-}
-
-function selectAnswer (e) {
-    var selectedButton = e.target
-    var correct = selectedButton.dataset.correct
-    setStatusClass(document.body, correct)
-    Array.from(answersButtonEl.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct)
-    })
-    if (shuffledQuestions.length > currentQuestionsIndex + 1) {
-        nextBtn.classList.remove("hide")
-    } else {
-        startBtn.innerText = "Restart"
-        startBtn.classList.remove("hide")
-    }
-    nextBtn.classList.remove("hide")
-}
-
-function setStatusClass(element, correct) {
-    clearStatusClass(element)
-    if (correct) {
-        element.classList.add("correct");
-    } else {
-        element.classList.add("wrong");
-    }
-}
-
-function clearStatusClass(element) {
-        element.classList.remove("correct");
-        element.classList.remove("wrong");
-}
+var index = 0;
 
 var question = [
     {
@@ -136,3 +43,44 @@ var question = [
         ]
     },
 ]
+
+//Timer counting down from 100 seconds
+function countdown() {
+    var timeLeft = 50;
+
+    var timeInterval = setInterval(function() {
+if (timeLeft > 1) {
+    timerEl.textContent = timeLeft;
+    timeLeft--;
+
+} else if (timeLeft === 1) {
+    timeLeft.textContent = timeLeft + " Keep fighting!";
+    timeLeft--;
+} else {
+    timerEl.textContent = "You Lose";
+    clearInterval(timeInterval);
+
+}   
+
+}, 1000);
+}
+function startGame() {
+    countdown();
+    startBtn.classList.add("hide");
+    questionContainerEl.innerHTML = ` 
+    <div id="questions">${question[index].question}</div>
+    <div id="answer-buttons" class="btn-grid">
+        <button value= "${question[index].answers[0].correct}" class="btn ansBtn">${question[index].answers[0].text}</button>
+        <button value= "${question[index].answers[1].correct}" class="btn ansBtn">${question[index].answers[1].text}</button>
+        <button value= "${question[index].answers[2].correct}" class="btn ansBtn">${question[index].answers[2].text}</button>
+        <button value= "${question[index].answers[3].correct}" class="btn ansBtn">${question[index].answers[3].text}</button>
+    </div>`
+    let answer = document.getElementsByClassName("ansBtn");
+    answer.addEventListener("click", function(event){
+        console.log("test")
+    })
+}
+
+
+
+
